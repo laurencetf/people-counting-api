d71151e64d547b362b27c7866c5c5d1424a17035
fabien laurencet <fabienlaurencet@hotmail.fr>
2019-05-04 18:27:47 +0200

    push

--- People-counting-API/People_counting_API/urls.py
+++ People-counting-API/People_counting_API/urls.py
@@ -8,6 +8,8 @@ from . import settings
 from django.conf.urls import url
 from django.conf.urls import include
 import django.contrib.auth.views
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^register/', include('Register.urls'), name='register'),
@@ -16,4 +18,5 @@ urlpatterns = [
     url(r'^project/', include('Project.urls'),name="project"),
     url(r'^clicks/', include('Clicks.urls'),name="clicks"),
     url(r'^test/', include('Test.urls'),name="test"),
+    url(r'^admin/', include(admin.site.urls))
 ]
