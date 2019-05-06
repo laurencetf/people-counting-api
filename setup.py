57e8ab4d2ad2fa03cd36d459677bd9a3585a2b6c
fabien laurencet <fabienlaurencet@hotmail.fr>
2019-05-06 22:41:56 +0200

    push

--- setup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-from setuptools import setup, find_packages
-
-setup(name='People-counting-API',
-      version='1.0',
-      packages=find_packages(),
-      scripts=['manage.py'])
