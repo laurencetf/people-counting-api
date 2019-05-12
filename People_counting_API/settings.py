2e6b05616a192f86552f18d868a08b1a0e88b5c5
fabien laurencet <fabienlaurencet@hotmail.fr>
2019-05-12 22:00:37 +0200

    push

--- People_counting_API/settings.py
+++ People_counting_API/settings.py
@@ -138,6 +138,9 @@ CSRF_COOKIE_SECURE = True
 
 SESSION_COOKIE_SECURE = True
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
