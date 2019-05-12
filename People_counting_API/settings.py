d0f83def5de6100ce303b3900f0c64facdcdcbe0
fabien laurencet <fabienlaurencet@hotmail.fr>
2019-05-13 00:21:09 +0200

    pupupush

--- People_counting_API/settings.py
+++ People_counting_API/settings.py
@@ -119,15 +119,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = (
-    'localhost:3030',
-)
-CORS_ORIGIN_REGEX_WHITELIST = (
-    'localhost:3030',
-)
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
