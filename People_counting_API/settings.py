0337511924abcededaf9c1c14fc51fced9315542
fabien laurencet <fabienlaurencet@hotmail.fr>
2019-05-12 22:29:52 +0200

    settings change

--- People_counting_API/settings.py
+++ People_counting_API/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.sessions.middleware.Session',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware'
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
