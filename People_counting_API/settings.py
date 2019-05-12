b8643c60102148eec5af5b8ffd35583ffad8ae55
fabien laurencet <fabienlaurencet@hotmail.fr>
2019-05-13 01:28:03 +0200

    add dependancies

--- People_counting_API/settings.py
+++ People_counting_API/settings.py
@@ -50,11 +50,11 @@ INSTALLED_APPS = [
     ]
 
 MIDDLEWARE_CLASSES = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.Session',
     'corsheaders.middleware.CorsMiddleware'
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
