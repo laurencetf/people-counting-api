7bf3188909cb8a604a99733133379cd649af9756
fabien laurencet <fabienlaurencet@hotmail.fr>
2019-05-14 20:42:46 +0200

    correct

--- People_counting_API/settings.py
+++ People_counting_API/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE_CLASSES = [
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 ]
